161242193226f0a66e1cbe7b585e003d875c0c17
mdtanvirahammed1 <mdtanvirahammed416@gmail.com>
2026-03-08 23:43:47 +0600

    add login authentication

---
+++ app.js
@@ -0,0 +1,12 @@
+function login(){
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    if(username === "admin" && password === "admin123"){
+        document.getElementById("loginPage").classList.add("hidden")
+        document.getElementById("dashboard").classList.remove("hidden")
+        loadIssues()
+}
+else{
+    alert("Invalid username or password")
+}
+}
